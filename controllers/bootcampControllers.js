7461f2a1501d1968acbf03d9e8ac8a7081175846
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-07-22 16:26:15 +0600

    pagination on  getBootcamps

--- controllers/bootcampControllers.js
+++ controllers/bootcampControllers.js
@@ -41,16 +41,44 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     const sortBy = req.query.sort.split(',').join(' ');
     query = query.sort(sortBy);
   } else {
-    query = query.sort('-createdAt')
+    query = query.sort('-createdAt');
   }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 20;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
 
   // const bootcamps = await Bootcamp.find().sort('_id');
   // Executing query
   const bootcamps = await query;
 
-  res
-    .status(200)
-    .json({ succes: true, count: bootcamps.length, data: bootcamps });
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+  res.status(200).json({
+    succes: true,
+    count: bootcamps.length,
+    pagination: pagination,
+    data: bootcamps,
+  });
 });
 
 // @desc      Get single bootcamp
