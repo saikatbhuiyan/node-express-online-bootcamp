07329358cc1916f55a883eb0ae7bcf0e6069ccab
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-07-24 01:07:18 +0600

    update and delate a course

--- route/coursesRoute.js
+++ route/coursesRoute.js
@@ -4,12 +4,15 @@ const {
   getCourses,
   getCourse,
   addCourse,
+  deleteCourse,
+  updateCourse,
 } = require('../controllers/courseControllers');
 
 // We merging the url param
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
