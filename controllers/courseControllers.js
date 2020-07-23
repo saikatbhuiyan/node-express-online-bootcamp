513684fa81bba922537ca524e73115e05937bf38
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-07-24 00:59:15 +0600

    get single course and create course

--- controllers/courseControllers.js
+++ controllers/courseControllers.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utlis/errorResponse');
 const asyncHandler = require('../async');
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc      Get all Course
 // @route     GET /api/v1/coueses
@@ -24,5 +25,50 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     succes: true,
     count: courses.length,
     data: courses,
+  });
+});
+
+// @desc      Get A Couse
+// @route     GET /api/v1/coueses/:id
+// @access    Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc      Add course
+// @route     POST /api/v1/bootcamps/:bootcampId/courses
+// @access    Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`),
+      404
+    );
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: course,
   });
 });
--- route/coursesRoute.js
+++ route/coursesRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
-const { getCourses } = require('../controllers/courseControllers');
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+} = require('../controllers/courseControllers');
 
 // We merging the url param
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addCourse);
+router.route('/:id').get(getCourse);
 
 module.exports = router;
