3f226cfb03ff6932605473c0a9997b6070e27d39
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-07-27 12:58:40 +0600

    Create token

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,5 +17,8 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ success: true });
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
--- models/User.js
+++ models/User.js
@@ -41,4 +41,11 @@ UserSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
 module.exports = mongoose.model('User', UserSchema);
